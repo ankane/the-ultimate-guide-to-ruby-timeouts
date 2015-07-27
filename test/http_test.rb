f5caf9f500d9de763696177b0103a926e3ed0f97
Andrew Kane <acekane1@gmail.com>
2015-07-27 00:25:02 -0700

    Simplified test

--- test/http_test.rb
+++ test/http_test.rb
@@ -3,13 +3,13 @@ require_relative "test_helper"
 class HttpTest < Minitest::Test
   def test_connect
     assert_timeout(HTTP::TimeoutError) do
-      HTTP.timeout(:per_operation, connect: 1).get(connect_url)
+      HTTP.timeout(connect: 1).get(connect_url)
     end
   end
 
   def test_read
     assert_timeout(HTTP::TimeoutError) do
-      HTTP.timeout(:per_operation, read: 1).get(read_url)
+      HTTP.timeout(read: 1).get(read_url)
     end
   end
 end
