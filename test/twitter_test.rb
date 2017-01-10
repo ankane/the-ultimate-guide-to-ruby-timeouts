a27c9752f63fa89437773fa6e1277b356910ecdb
Andrew Kane <acekane1@gmail.com>
2017-01-10 02:21:25 -0800

    Updated twitter tests

--- test/twitter_test.rb
+++ test/twitter_test.rb
@@ -2,9 +2,18 @@ require_relative "test_helper"
 
 class TwitterTest < Minitest::Test
   def test_connect
-    skip
-    Twitter::REST::Client.send(:remove_const, "BASE_URL")
-    Twitter::REST::Client.const_set("BASE_URL", connect_url)
-    Twitter::REST::Client.new(timeouts: {connect: 1}).followers
+    assert_raises(HTTP::TimeoutError) do
+      Twitter::REST::Request.send(:remove_const, "BASE_URL")
+      Twitter::REST::Request.const_set("BASE_URL", connect_url)
+      Twitter::REST::Client.new(timeouts: {connect: 1}).followers
+    end
+  end
+
+  def test_read
+    assert_raises(HTTP::TimeoutError) do
+      Twitter::REST::Request.send(:remove_const, "BASE_URL")
+      Twitter::REST::Request.const_set("BASE_URL", read_url)
+      Twitter::REST::Client.new(timeouts: {read: 1}).followers
+    end
   end
 end
