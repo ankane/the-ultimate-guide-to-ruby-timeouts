c13cef051cf6c13bafe25c88b4f0fd4127233be9
Andrew Kane <acekane1@gmail.com>
2017-01-29 03:53:04 -0800

    Cleaned curb test [skip ci]

--- test/curb_test.rb
+++ test/curb_test.rb
@@ -2,8 +2,7 @@ require_relative "test_helper"
 
 class CurbTest < Minitest::Test
   def test_connect
-    # times out early on Travis
-    assert_timeout(Curl::Err::TimeoutError, timeout: 0.99) do
+    assert_timeout(Curl::Err::TimeoutError, timeout: timeout) do
       curl = Curl::Easy.new(connect_url)
       curl.connect_timeout = 1
       curl.perform
@@ -11,8 +10,7 @@ class CurbTest < Minitest::Test
   end
 
   def test_connect_timeout_option
-    # times out early on Travis
-    assert_timeout(Curl::Err::TimeoutError, timeout: 0.99) do
+    assert_timeout(Curl::Err::TimeoutError, timeout: timeout) do
       curl = Curl::Easy.new(connect_url)
       curl.timeout = 1
       curl.perform
@@ -20,11 +18,15 @@ class CurbTest < Minitest::Test
   end
 
   def test_read
-    # times out early on Travis
-    assert_timeout(Curl::Err::TimeoutError, timeout: 0.99) do
+    assert_timeout(Curl::Err::TimeoutError, timeout: timeout) do
       curl = Curl::Easy.new(read_url)
       curl.timeout = 1
       curl.perform
     end
+  end
+
+  def timeout
+    # times out early on Travis
+    travis? ? 0.99 : 1
   end
 end
