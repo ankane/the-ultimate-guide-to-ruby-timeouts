7f05083a3aa77de5e0b5034e24df45b440d6c442
Andrew Kane <acekane1@gmail.com>
2017-05-15 21:08:46 -0700

    Updated rest-client tests - #15

--- test/rest_client_test.rb
+++ test/rest_client_test.rb
@@ -2,14 +2,14 @@ require_relative "test_helper"
 
 class RestClientTest < Minitest::Test
   def test_connect
-    assert_timeout(RestClient::RequestTimeout) do
+    assert_timeout(RestClient::Exceptions::OpenTimeout) do
       RestClient::Request.execute(method: :get, url: connect_url, open_timeout: 1)
     end
   end
 
   def test_read
-    assert_timeout(RestClient::RequestTimeout) do
-      RestClient::Request.execute(method: :get, url: read_url, timeout: 1)
+    assert_timeout(RestClient::Exceptions::ReadTimeout) do
+      RestClient::Request.execute(method: :get, url: read_url, read_timeout: 1)
     end
   end
 end
