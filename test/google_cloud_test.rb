9326a973ac7f09e024970a24ae874b6aeedc5b52
Andrew Kane <andrew@chartkick.com>
2018-12-14 21:08:23 -0800

    Better test [skip ci]

--- test/google_cloud_test.rb
+++ test/google_cloud_test.rb
@@ -7,17 +7,19 @@ class GoogleCloudTest < Minitest::Test
   end
 
   def test_connect
+    storage = Google::Cloud::Storage.new(project: "test", timeout: 1, retries: 0)
+    storage.service.service.root_url = connect_url + "/"
+
     assert_timeout(Google::Cloud::Error) do
-      storage = Google::Cloud::Storage.new(project: "test", timeout: 1, retries: 0)
-      storage.service.service.root_url = connect_url + "/"
       storage.buckets
     end
   end
 
   def test_read
+    storage = Google::Cloud::Storage.new(project: "test", timeout: 1, retries: 0)
+    storage.service.service.root_url = read_url + "/"
+
     assert_timeout(Google::Cloud::Error) do
-      storage = Google::Cloud::Storage.new(project: "test", timeout: 1, retries: 0)
-      storage.service.service.root_url = read_url + "/"
       storage.buckets
     end
   end
