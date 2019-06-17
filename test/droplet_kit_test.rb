5c898d4e16269d533b95eea82b8dffd78fd5ce2a
Andrew Kane <andrew@chartkick.com>
2019-06-16 22:22:48 -0700

    Removed inspect [skip ci]

--- test/droplet_kit_test.rb
+++ test/droplet_kit_test.rb
@@ -3,7 +3,7 @@ require_relative "test_helper"
 class DropletKitTest < Minitest::Test
   def test_connect
     assert_timeout(Faraday::ConnectionFailed) do
-      p DropletKit::Client.new(api_url: connect_url, open_timeout: 1).droplets.find(id: "id")
+      DropletKit::Client.new(api_url: connect_url, open_timeout: 1).droplets.find(id: "id")
     end
   end
 
