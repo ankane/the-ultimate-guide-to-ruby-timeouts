292c5cbc355b84237d95b5557b8ef061b9bfcd4f
Andrew Kane <andrew@chartkick.com>
2020-03-18 22:25:43 -0700

    Fixed Mongoid test

--- test/mongoid_test.rb
+++ test/mongoid_test.rb
@@ -13,7 +13,7 @@ class MongoidTest < Minitest::Test
       }
     })
 
-    assert_timeout(Mongo::Error::NoServerAvailable, timeout: 4) do
+    assert_timeout(Mongo::Error::NoServerAvailable, timeout: 2) do
       Mongoid::Clients.default[:artists].find.count
     end
   end
