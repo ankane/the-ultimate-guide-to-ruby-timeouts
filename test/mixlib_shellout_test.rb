5885cc78c598d9fc427e790d840f124f7a854b5c
Andrew Kane <andrew@chartkick.com>
2020-07-24 15:53:14 -0700

    Improved mixlib-shellout test [skip ci]

--- test/mixlib_shellout_test.rb
+++ test/mixlib_shellout_test.rb
@@ -2,12 +2,13 @@ require_relative "test_helper"
 
 class MixlibShelloutTest < Minitest::Test
   def test_timeout
-    command = "for i in `seq 1 10`; do echo $i; sleep 1.2; done"
+    command = "for i in `seq 1 100`; do sleep 0.1; echo $i; done"
     shell = Mixlib::ShellOut.new(command, timeout: 1)
     # there are a few seconds of setup time, so set timeout to 4
     assert_timeout(Mixlib::ShellOut::CommandTimeout, timeout: 4) do
       shell.run_command
     end
-    assert_equal "1\n", shell.stdout
+    # can go slightly over
+    refute_match "13", shell.stdout
   end
 end
