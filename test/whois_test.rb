e5b4b0db9c0e2b77dde8225e94e3f0aa0106a3c2
Andrew Kane <andrew@ankane.org>
2021-10-03 22:08:32 -0700

    Fixed whois read test [skip ci]

--- test/whois_test.rb
+++ test/whois_test.rb
@@ -8,10 +8,8 @@ class WhoisTest < Minitest::Test
   end
 
   def test_read
-    skip # no way to add port
-
     assert_timeout(Timeout::Error) do
-      Whois::Client.new(host: read_host_and_port, timeout: 1).lookup("example.com")
+      Whois::Client.new(host: read_host, port: read_port, timeout: 1).lookup("example.com")
     end
   end
 end
