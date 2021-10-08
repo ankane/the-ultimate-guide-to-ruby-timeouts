9f90a49e37e0bd049723d9b654a6a25a5a73b63d
Andrew Kane <andrew@ankane.org>
2021-10-08 13:20:50 -0700

    Fixed mlanett-redis-lock test [skip ci]

--- test/mlanett_redis_lock_test.rb
+++ test/mlanett_redis_lock_test.rb
@@ -4,7 +4,7 @@ class MlanettRedisLockTest < Minitest::Test
   def test_acquire
     redis = Redis.new
     redis.lock("test", life: 1, acquire: 1, owner: "pid1")
-    assert_timeout(Redis::Lock::LockNotAcquired) do
+    assert_timeout(Redis::Lock::LockNotAcquired, timeout: 0.89) do
       redis.lock("test", life: 1, acquire: 1, owner: "pid2")
     end
   end
