522cfc392a53cd589f0870161ff6ab8c977b8c3e
Andrew Kane <andrew@ankane.org>
2021-10-08 13:58:36 -0700

    Fixed thrift_client test [skip ci]

--- test/thrift_client_test.rb
+++ test/thrift_client_test.rb
@@ -2,20 +2,27 @@ require_relative "test_helper"
 
 class TestClient
   include Thrift::Client
+
+  def ping
+    receive_message(nil)
+  end
+
+  def send_ping
+  end
 end
 
 class ThriftClientTest < Minitest::Test
   def test_connect
     client = ThriftClient.new(TestClient, "#{connect_host}:9160", retries: 0, connect_timeout: 1)
     assert_timeout(ThriftClient::NoServersAvailable) do
-      client.connect!
+      client.ping
     end
   end
 
   def test_read
     client = ThriftClient.new(TestClient, read_host_and_port, retries: 0, timeout: 1)
-    assert_timeout(Thrift::TransportException) do
-      client.connect!
+    assert_timeout(TestClient::TransportException) do
+      client.ping
     end
   end
 end
