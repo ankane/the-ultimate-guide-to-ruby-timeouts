26544f4f11306a2af2a89e3d24fc4155d9b9b1fa
Phil Nash <philnash@gmail.com>
2022-11-18 11:41:55 +0800

    Updates Bitly to show how to set timeouts in version 3

--- test/bitly_test.rb
+++ test/bitly_test.rb
@@ -1,25 +1,26 @@
 require_relative "test_helper"
+require "net/http"
 
 class BitlyTest < Minitest::Test
   def test_connect
-    skip "Not configurable"
-
     Bitly::API.send(:remove_const, "BASE_URL")
     Bitly::API.const_set("BASE_URL", URI(connect_url))
 
-    # assert_timeout(BitlyTimeout) do
-    #   Bitly::API::Client.new(token: "123").shorten(long_url: "http://bit.ly/wQaT")
-    # end
+    assert_timeout(Net::OpenTimeout) do
+      adapter = Bitly::HTTP::Adapters::NetHTTP.new(request_opts: { open_timeout: 1 })
+      http_client = Bitly::HTTP::Client.new(adapter)
+      Bitly::API::Client.new(token: "123", http: http_client).shorten(long_url: "http://bit.ly/wQaT")
+    end
   end
 
   def test_read
-    skip "Not configurable"
-
     Bitly::API.send(:remove_const, "BASE_URL")
     Bitly::API.const_set("BASE_URL", URI(read_url))
 
-    # assert_timeout(BitlyTimeout) do
-    #   Bitly::API::Client.new(token: "123").shorten(long_url: "http://bit.ly/wQaT")
-    # end
+    assert_timeout(Net::ReadTimeout) do
+      adapter = Bitly::HTTP::Adapters::NetHTTP.new(request_opts: { read_timeout: 1, use_ssl: false })
+      http_client = Bitly::HTTP::Client.new(adapter)
+      Bitly::API::Client.new(token: "123", http: http_client).shorten(long_url: "http://bit.ly/wQaT")
+    end
   end
 end
