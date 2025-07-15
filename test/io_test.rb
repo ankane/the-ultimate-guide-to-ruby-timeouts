bebe084ba6966990d8503bfe3c6bbbea9c4d446e
Andrew Kane <andrew@ankane.org>
2025-07-15 15:22:23 -0700

    Removed unneeded code [skip ci]

--- test/io_test.rb
+++ test/io_test.rb
@@ -1,10 +1,6 @@
 require_relative "test_helper"
 
 class IoTest < Minitest::Test
-  def setup
-    skip if RUBY_VERSION.to_f < 3.2
-  end
-
   def test_timeout
     previous_timeout = STDIN.timeout
     begin
--- test/regexp_test.rb
+++ test/regexp_test.rb
@@ -1,10 +1,6 @@
 require_relative "test_helper"
 
 class RegexpTest < Minitest::Test
-  def setup
-    skip if RUBY_VERSION.to_f < 3.2
-  end
-
   def test_timeout
     assert_timeout(Regexp::TimeoutError) do
       Regexp.new(/^a*b?a*()\1$/, timeout: 1) =~ "a" * 50000 + "x"
