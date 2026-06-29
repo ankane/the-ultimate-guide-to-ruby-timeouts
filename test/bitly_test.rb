638a1a3ccc811616f95223226007d946ae0da3b7
Andrew Kane <andrew@ankane.org>
2026-06-29 13:06:50 -0700

    Updated style [skip ci]

--- test/bitly_test.rb
+++ test/bitly_test.rb
@@ -7,7 +7,7 @@ class BitlyTest < Minitest::Test
     Bitly::API.const_set("BASE_URL", URI(connect_url))
 
     assert_timeout(Net::OpenTimeout) do
-      adapter = Bitly::HTTP::Adapters::NetHTTP.new(request_opts: { open_timeout: 1 })
+      adapter = Bitly::HTTP::Adapters::NetHTTP.new(request_opts: {open_timeout: 1})
       http_client = Bitly::HTTP::Client.new(adapter)
       Bitly::API::Client.new(token: "123", http: http_client).shorten(long_url: "https://bit.ly/wQaT")
     end
@@ -18,7 +18,7 @@ class BitlyTest < Minitest::Test
     Bitly::API.const_set("BASE_URL", URI(read_url))
 
     assert_timeout(Net::ReadTimeout) do
-      adapter = Bitly::HTTP::Adapters::NetHTTP.new(request_opts: { read_timeout: 1, use_ssl: false })
+      adapter = Bitly::HTTP::Adapters::NetHTTP.new(request_opts: {read_timeout: 1, use_ssl: false})
       http_client = Bitly::HTTP::Client.new(adapter)
       Bitly::API::Client.new(token: "123", http: http_client).shorten(long_url: "https://bit.ly/wQaT")
     end
