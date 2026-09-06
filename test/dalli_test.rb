cc136628aa81800130437693f1320eae6ffdb665
Andrew Kane <andrew@ankane.org>
2026-09-06 16:32:09 -0700

    Fixed dalli tests [skip ci]

--- test/dalli_test.rb
+++ test/dalli_test.rb
@@ -3,19 +3,19 @@ require_relative "test_helper"
 class DalliTest < Minitest::Test
   def test_connect
     assert_timeout(Dalli::RingError) do
-      Dalli::Client.new(connect_host, socket_timeout: 1).get("hi")
+      Dalli::Client.new(connect_host, socket_timeout: 1, socket_max_failures: 1).get("hi")
     end
   end
 
   def test_connect_default
     assert_timeout(Dalli::RingError, timeout: 1) do
-      Dalli::Client.new(connect_host).get("hi")
+      Dalli::Client.new(connect_host, socket_max_failures: 1).get("hi")
     end
   end
 
   def test_read
     assert_timeout(Dalli::RingError) do
-      Dalli::Client.new(read_host_and_port, socket_timeout: 1).get("hi")
+      Dalli::Client.new(read_host_and_port, socket_timeout: 1, socket_max_failures: 1).get("hi")
     end
   end
 end
